f655d075aece5bd111c8c228b8bac1c54bcbc823
Simão Neto <simaomenezesneto@gmail.com>
2024-02-22 21:34:11 +0000

    feat: Create component Input

--- MyToDoList/src/App.jsx
+++ MyToDoList/src/App.jsx
@@ -1,14 +1,21 @@
 import { useState } from 'react'
 
-import stayles from './App.module.css'
+import styles from './App.module.css'
 import { Header } from './components/Header'
+import { Input } from './components/Input'
 
 export default function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
+    <>
       <Header/>
-    </div>
+
+      <section className={styles.content}>
+        <div className={styles.taskInfoContainer}>
+          <Input/>
+        </div>        
+      </section>
+    </>
   )
 }
